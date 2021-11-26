fccfbadbb1aa40c19e4ad8045ded7cb5659a2cb0
Hadi Ahmadi <ahmadi.hadi@outlook.com>
2021-11-26 21:03:23 +0330

    writing the first property, without doing any change on the target class

--- code/src/MovieRentalStore/MovieRental.cs
+++ code/src/MovieRentalStore/MovieRental.cs
@@ -58,7 +58,6 @@ namespace MovieRentalStore
                             var totalRentalFee = rentalCostPerDay * DaysRented;
                             customer.Charge(totalRentalFee);
                         }
-
                         break;
                     }
             }
